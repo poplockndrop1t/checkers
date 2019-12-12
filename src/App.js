65c7c60c70a8dea65dcdf04b8c81ecf43907b77a
woonandez <matthew.allen.fernandez@gmail.com>
2019-12-12 10:11:42 -0800

    Add game board

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// components
+import Board from './game/board.js';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Board />
     </div>
   );
 }
 
 export default App;
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
---
+++ src/game/board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './board.css';
+
+
+const Board = () => (
+  <div>
+    <h1>Game Board</h1>
+  </div>
+);
+
+export default Board;
+
